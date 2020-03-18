e38461098bed7b62928c036ce5e384fa4de86b27
Alex Roan <alex.roan@hotmail.com>
2020-03-18 16:33:24 +0000

    Updated truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -7,8 +7,8 @@ module.exports = {
   networks: {
     develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
       host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
+      port: 7545,
+      network_id: "5777"
     }
   }
 };
