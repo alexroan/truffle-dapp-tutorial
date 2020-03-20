88468813cc7609ae3d57be2980b7b29de08a88dc
Alex Roan <alex.roan@hotmail.com>
2020-03-20 15:25:47 +0000

    Updated deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const HelloWorld = artifacts.require("HelloWorld");
+const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
   deployer.deploy(HelloWorld);
+  deployer.deploy(SimpleStorage);
 };
